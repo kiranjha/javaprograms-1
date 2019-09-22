8434b6b51c86cf635d1229569ebdcae4302fcf39
Apoorv Nag <apoorv@tripoto.com>
2019-09-22 11:59:54 +0530

    ExceptionHandling Demo

---
+++ src/sep22/ExceptionHandlingDemo.java
@@ -0,0 +1,12 @@
+package sep22;
+
+public class ExceptionHandlingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d = 12;
+		int t = d / 0;
+		System.out.println(t);
+	}
+
+}
